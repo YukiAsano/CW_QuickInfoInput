4418eab211ef691cd20e60c9065afa4a71a61db3
Yuki Asano <y.asano0123@gmail.com>
2016-04-14 23:51:04 +0900

    1.8

--- ChatWorkQuickInfoInput/scripts/core.js
+++ ChatWorkQuickInfoInput/scripts/core.js
@@ -45,14 +45,12 @@ function getButtonEl(args){
     // {{{ ボタンの中身を生成
 
     innerEl = document.createElement("span");
-    innerEl.className = args.iconNoLg ? "" : "icoSizeLarge";
+    innerEl.className = args.iconCls;
     innerEl.style.color = args.color || undefined;
     innerEl.innerHTML = args.html ? args.html : "";
 
-
     // ボタンによって変える部分
-    innerEl.className += " ";
-    innerEl.className += args.iconCls;
+    innerEl.className += args.iconNoLg ? "" : " icoSizeLarge";
 
     // }}}
     // {{{ 中身を入れて、返す
@@ -75,10 +73,10 @@ if (isChatPage()) {
         qtBtn,
         hrBtn,
         chatToolbarEl = document.getElementById("_chatSendTool"),
-        actionFn = function(action, bTitle, noEnd) {
+        actionFn = function(action, bTitle, bNoEnd) {
             var el,
                 startTag = "["+action+"]",
-                endTag = noEnd ? "" : "[/"+action+"]",
+                endTag = bNoEnd ? "" : "[/"+action+"]",
                 startTtlTag = "[title]",
                 endTtlTag = "[/title]",
                 ttlText,
@@ -126,94 +124,112 @@ if (isChatPage()) {
             // カーソル位置の移動
             el.setSelectionRange(newPoint, newPoint);
 
+            // フォーカスを当ててそのまま送信できるように
+            el.focus();
         };
 
-    // infoタグ生成のボタン
+    // {{{ infoタグ生成のボタン
+
     infoBtn = getButtonEl({
         id: "_infoText",
-        label: "メッセージに[info][/info]を追加します",
-        iconCls: "icoFontAdminInfoMenu"
+        label: "メッセージに[info][/info]を追加します（Ctrl+Alt+i）",
+        iconCls: "icoFontInfo"
     });
 
-    infoBtn.addEventListener('click', function() {
-        actionFn('info', false);
+    infoBtn.addEventListener("click", function() {
+        actionFn("info", false, false);
     }, false);
 
     chatToolbarEl.appendChild(infoBtn);
 
-    // infoタグ生成のボタン
+    // }}}
+    // {{{ infoタグ（タイトル付き）生成のボタン
+
     infoWithTitleBtn = getButtonEl({
-        id: "_infoText",
-        label: "メッセージに[info][title][/title][/info]を追加します",
-        iconCls: "icoFontAdminInfoMenu",
+        id: "_infoWithTitleText",
+        label: "メッセージに[info][title][/title][/info]を追加します（Ctrl+Alt+t）",
+        iconCls: "icoFontInfo",
         color: "blue"
     });
 
-    infoWithTitleBtn.addEventListener('click', function() {
-        actionFn('info', true);
+    infoWithTitleBtn.addEventListener("click", function() {
+        actionFn("info", true, false);
     }, false);
 
     chatToolbarEl.appendChild(infoWithTitleBtn);
 
-    // codeタグ生成のボタン
+    // }}}
+    // {{{ codeタグ生成のボタン
+
     codeBtn = getButtonEl({
-        id: "_insertCode",
-        label: "メッセージに[code][/code]を追加します",
+        id: "_insertCodeText",
+        label: "メッセージに[code][/code]を追加します（Ctrl+Alt+w）",
         iconCls: "icoFontSetting"
     });
 
-    codeBtn.addEventListener('click', function() {
-        actionFn('code', false);
+    codeBtn.addEventListener("click", function() {
+        actionFn("code", false, false);
     }, false);
 
     chatToolbarEl.appendChild(codeBtn);
 
-    // qtタグ生成のボタン
+    // }}}
+    // {{{ qtタグ生成のボタン
+
     qtBtn = getButtonEl({
-        id: "_insertCode",
+        id: "_insertQtText",
         label: "メッセージに[qt][/qt]を追加します",
         iconCls: "icoFontMessegeQuote",
         iconNoLg: true
     });
 
-    qtBtn.addEventListener('click', function() {
-        actionFn('qt', false);
+    qtBtn.addEventListener("click", function() {
+        actionFn("qt", false, false);
     }, false);
 
     chatToolbarEl.appendChild(qtBtn);
 
-    // hrタグ生成のボタン
+    // }}}
+    // {{{ hrタグ生成のボタン
+
     hrBtn = getButtonEl({
-        id: "_insertCode",
-        label: "メッセージに[hr]を追加します",
+        id: "_insertHrText",
+        label: "メッセージに[hr]を追加します（Ctrl+Alt+l）",
         iconCls: "btnPrimary toolTip",
         iconNoLg: true,
         html: "&nbsp;hr&nbsp;"
     });
 
-    hrBtn.addEventListener('click', function() {
-        actionFn('hr', false, true);
+    hrBtn.addEventListener("click", function() {
+        actionFn("hr", false, true);
     }, false);
 
     chatToolbarEl.appendChild(hrBtn);
 
-    // キーボードショートカット
-    document.getElementById('_chatText').addEventListener('keydown', function(e) {
+    // }}}
+    // {{{ キーボードショートカット
+
+    document.getElementById("_chatText").addEventListener("keydown", function(e) {
         var code = e.which,
             keyChar = String.fromCharCode(code).toLowerCase();
         if (e.ctrlKey) {
-            if (keyChar === 'i') {
+            if (keyChar === "i") {
                 // info追加
-                actionFn('info', false);
-            } else if (keyChar === 't') {
+                actionFn("info", false, false);
+            } else if (keyChar === "t") {
                 // titleつきでinfo追加
-                actionFn('info', true);
-            } else if (keyChar === 'w') {
+                actionFn("info", true, false);
+            } else if (keyChar === "w") {
                 // code追加
-                actionFn('code', false);
+                actionFn("code", false, false);
+            } else if (keyChar === "l") {
+                // hr追加
+                actionFn("hr", false, true);
             }
         }
     }, false);
+
+    // }}}
 }
 
 // }}}
